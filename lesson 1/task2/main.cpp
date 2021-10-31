f4e8f6ecd0d0c4aa462d84c71a2d82173441acb2
Daniil Berestenko <berestenkodaniil2004@gmail.com>
2021-10-31 14:55:35 +0200

    task 2 added

---
+++ lesson 1/task2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << " /|_/| \n" ;
+    cout << "( 0.0 ) \n";
+    cout << " > ^ < \n";
+    cout << "(     ) \n";
+    cout << "/     | ";
+    return 0;
+}
