b868c42784950f7cfdb3fb4577a3361fced03c03
Daniil Berestenko <berestenkodaniil2004@gmail.com>
2021-10-31 20:24:21 +0200

    task 5 updated

--- lesson 1/task5/main.cpp
+++ lesson 1/task5/main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main()
 {
     int a;
+    int b;
     cout << "Enter starship weight: ";
     cin >> a;
-    a=((a/3)-2)*300;
-    cout << "THis amount of fuel " << a << " You need to travel for 300 years";
+    cout << "Enter number of years you are going to travel: ";
+    cin >> b;
+    a=((a/3)-2)*b;
+    cout << "THis amount of fuel " << a << " You need to travel for " << b << " years";
     return 0;
 }
