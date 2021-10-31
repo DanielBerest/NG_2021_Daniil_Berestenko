b59a14bd75134ff99449d3b0b9dd92a1dbaaad72
Daniil Berestenko <berestenkodaniil2004@gmail.com>
2021-10-31 15:39:19 +0200

    added task four

---
+++ lesson 1/task4/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+   double a,b,c,D,x1,x2;
+   cout << "Write down the value of the number a:  ";
+   cin >> a;
+   cout << "Write down the value of the number b:  ";
+   cin >> b;
+   cout << "Write down the value of the number c:  ";
+   cin >> c;
+   D=b*b-4*a*c;
+        if (D=0){
+            cout << D;
+        }
+        if (D<0){
+            cout << "There is no roots.....";
+        }
+        if (D>0){
+                x1=( -1*b + sqrt(D) ) / (2 * a);
+                x2=( -1*b - sqrt(D) ) / (2 * a);
+                cout << "First root: " ;
+                cout << x1 << endl;
+                cout << "Second root: ";
+                cout << x2;
+   }
+    return 0;
+}
